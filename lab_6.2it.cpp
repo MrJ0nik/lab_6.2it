ece8f252e7e2b7e11107a0f7107b3e9791de7b17
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-10-08 22:58:37 +0300

    first commit

---
+++ lab_6.2it.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <random>
+
+using namespace std;
+
+void randomnum(int a[], int size) {
+    const int MIN_VALUE = 1;
+    const int MAX_VALUE = 100;
+
+    random_device rd;
+    mt19937 generator(rd());
+    uniform_int_distribution<int> distribution(MIN_VALUE, MAX_VALUE);
+
+    for (int i = 0; i < size; ++i) {
+        a[i] = distribution(generator);
+    }
+}
+
+void printArray(int a[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << setw(4) << a[i];
+    }
+    cout << endl;
+}
+
+
+int sumArray(int a[], int size, int& count, int& sum) {
+    count = sum = 0;  
+    for (int i = 0; i < size; i++) {
+        if (!(a[i] % 2 == 0)) {
+            ++count;
+            sum += a[i];
+        }
+    }
+    return sum; 
+}
+
+int main() {
+    const int size = 10;
+    int a[size]; 
+
+
+    randomnum(a, size);
+
+
+    cout << "Array elements: ";
+    printArray(a, size);
+
+    int count, sum;
+
+
+    sumArray(a, size, count, sum);
+
+
+    cout << "Number of even elements: " << count << endl;
+    cout << "Sum of even elements: " << sum << endl;
+
+    return 0;
+}
